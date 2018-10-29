7f7b1c2e774cbf491a9271e8daaa309d273ae40b
Jason Snyder <jasonsnyder@10.0.0.59>
2018-10-28 21:06:09 -0400

    Began Creating Tiles, added Tile Images

--- Catan/src/com/jason/Board.java
+++ Catan/src/com/jason/Board.java
@@ -1,14 +1,37 @@
 package com.jason;
 
 import java.util.ArrayList;
+import javafx.stage.Stage;
 
 public class Board {
+	private final int NUMBER_OF_HEXES = 19;
+	public int intersections[];
+	private Stage primaryStage;
+	private double width;
+	private double height;
+	private double middle;
 	
-	public int intersection[];
 	
 	private ArrayList<Tile> tiles = new ArrayList<>();
 	
-	public Board() {
+	public Board(Stage primaryStage, double width, double height) {
+		this.primaryStage = primaryStage;
+		this.width = width;
+		this.height = height;
+		middle = width/2.0;
+		setTiles();
+		setChits();
+	}
+	
+	private void setTiles() {
+		for(int i = 0; i < NUMBER_OF_HEXES; i++) {
+			
+			Tile tile = new Tile(middle, primaryStage);
+		}
+
+	}
+	
+	private void setChits() {
 		
 	}
 }
